a78cc0257a9dc49b566c332e6380fa810efc5652
dush <duservice@live.cn>
2017-01-10 13:10:16 +0800

    添加搜索引擎和支持子文件夹（只支持一级）

--- newtab.js
+++ newtab.js
@@ -1,27 +1,89 @@
 var bookmarks = chrome.bookmarks;
 function readBooks() {
-	var box = null;
-	bookmarks.search("favorites", function(items) {
-		box = items[0];
-		bookmarks.getChildren(box.id, function(children) {
-			var tmp = $("#siteTemp").html();
-			var str = "";
-			for (var i = 0; i < children.length; i++) {
-				str = tmp.replace(/#url#/g, children[i].url);
-				str = str.replace(/#title#/g, children[i].title);
-				str = str.replace(/#icon#/g, getIcon(children[i].url));
-				$("#panel").append(str);
-			}
+    var box = null;
+    var createPanel = function (node) {
+        var tmp = $("#siteSetTemp").html();
+        str = tmp.replace(/#id#/g, node.id);
+        str = str.replace(/#title#/g, node.title);
+        return str;
+    };
+    var createSite = function (node) {
+        bookmarks.getChildren(node.id, function (children) {
+            var tmp = $("#siteTemp").html();
 
-		});
-	});
+            var str = "";
+            var panel = createPanel(node);
+            panel = $(panel);
+
+            var site = null;
+            for (var i = 0; i < children.length; i++) {
+                site = children[i];
+                if (site.url == null || site.url == "") {
+                    continue;
+                }
+
+                str = tmp.replace(/#url#/g, site.url);
+                str = str.replace(/#title#/g, site.title);
+                str = str.replace(/#icon#/g, getIcon(site));
+                panel.append(str);
+            }
+            panel.find("img").bind("error", function () {
+                $(this).attr('src', 'default.png');
+                console.log("error");
+            });
+            $("#panel").append(panel);
+        });
+    };
+    bookmarks.search("favorites", function (items) {
+        box = items[0];
+        box.title = "常用站点";
+        createSite(box);
+        bookmarks.getChildren(box.id, function (children) {
+            var site = null;
+            for (var i = 0; i < children.length; i++) {
+                site = children[i];
+                if (site.url == null || site.url == "") {
+                    createSite(site);
+                }
+            }
+        });
+    });
 }
-function getIcon(url) {
-	var us = url.split("/");
-	console.log(us);
-	iconUrl = us[0] + "//" + us[2] + "/favicon.ico";
-	return iconUrl;
+function getIcon(site) {
+    var us = site.url.split("/");
+    iconUrl = us[0] + "//" + us[2] + "/favicon.ico";
+    return iconUrl;
 }
-$(function() {
-	readBooks();
-})
+function getIconByNet(url) {
+    var ret;
+    $.ajax({
+        url: getIconUrl(url),
+        type: "get",
+        async: false,
+        success: function (d, p) {
+
+        }
+    });
+
+    return ret;
+}
+function getIconByStore(id) {
+    var store = localStorage;
+    var icons = store.getItem("iconSet");
+    icons = $.parseJSON(icons);
+    return icons[id];
+}
+function getIconUrl(url) {
+    var us = url.split("/");
+    console.log(us);
+    iconUrl = us[0] + "//" + us[2] + "/favicon.ico";
+    return iconUrl;
+}
+$(function () {
+    readBooks();
+    $("#search").bind("click", function () {
+        $("#searcher").submit();
+    });
+
+
+});
